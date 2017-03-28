5eb3aae4cae67b84be190b3953201687f085b4d5
pauligno <pauligno@gmail.com>
2017-03-28 18:06:08 +0200

    récupération token dans composant search.js Intérogation de l'api pour les villes mais cette erreur apparait {"error":"Zipcode must not be empty"}

--- reacttest/src/Search.js
+++ reacttest/src/Search.js
@@ -17,9 +17,39 @@ const stylebtnrecherche= {
  * The input is used to create the `dataSource`, so the input always matches three entries.
  */
 export default class AutoCompleteExampleSimple extends Component {
+  
+  //les propriétés du composant
   state = {
+    datatoken: null,
     dataSource: [],
+    dataVille: [],
   };
+  
+  componentDidMount() {
+    //avoir le token au chargement du composant
+    fetch("https://api.integration.eusko.meta-it.fr/api-token-auth/", { 
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: 'demo', password: 'demo'})
+    })
+    .then(function(response) {
+        return response.json() 
+    })
+    .then( (json) => {
+        //sauver le token dans le composant
+        this.setState({
+           datatoken: json.token
+        })
+        console.log('parsed json', json)
+    }).then(function(body) {
+        console.log(body);
+    });
+    
+  }// end componentDidMount
+  
+  
 
   handleUpdateInput = (value) => {
     this.setState({
@@ -29,13 +59,36 @@ export default class AutoCompleteExampleSimple extends Component {
         value + value + value,
       ],
     });
-  };
-
+  }; 
+  
+  chercherVilles = (value) => {
+    fetch("https://api.integration.eusko.meta-it.fr/towns/", { 
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Token '+this.state.datatoken
+      },
+    })
+    .then(function(response) {
+        return response.json() 
+    })
+    .then( (json) => {
+        //sauver le token dans le composant
+        this.setState({
+           dataVille: json
+        })
+        console.log('parsed json', json)
+    }).then(function(body) {
+        console.log(body);
+    });
+  }
+  
+  
   render() {
     return (
       <div>
         <Paper zDepth={2} >
-        
+        token = {this.state.datatoken}
         <TextField
             floatingLabelText="Mot clés"
             hintText="Journal"
@@ -57,8 +110,8 @@ export default class AutoCompleteExampleSimple extends Component {
         <Divider />
         <AutoComplete
             hintText="Donibane garazi 64220"
-            dataSource={this.state.dataSource}
-            onUpdateInput={this.handleUpdateInput}
+            dataSource={this.state.dataVille}
+            onUpdateInput={this.chercherVilles}
             style={style}
             floatingLabelText="Ville (CP)"
             underlineShow={false} 
